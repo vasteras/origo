9666d0e9ec62d771fdc7623ef8ab0a5335ed1274
daRasmussen <daniel.rasmussen@vasteras.se>
2019-10-07 13:04:11 +0200

    Adds a second param to animation called zoom2

--- src/controls/animation.js
+++ src/controls/animation.js
@@ -1,6 +1,7 @@
 const params = {};
 function init(opt) {
   params.zoom = opt.zoom;
+  params.zoom2 = opt.zoom2;
   params.duration = opt.duration;
   params.offsetX = opt.offsetX;
   params.offsetY = opt.offsetY;
